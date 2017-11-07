588be40671e982b657c3637eaf40a3fbc36a8495
KanayaMaki <chlminami64@gmail.com>
2017-11-08 00:16:27 +0900

    main.cppを追加

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <fstream>
-
 #include <Windows.h>
 
 #include "Atom/atom.h"
@@ -8,15 +5,6 @@
 using namespace cpot;
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszArgs, int nWinMode) {
-
-	std::fstream f;
-	f.open("a.txt");
-
-	//Ç‡ÇµäJÇØÇ»Ç©Ç¡ÇΩÇÁ
-	if (!f) {
-		//ÉGÉâÅ[
-		return 0;
-	}
 
 	Color c = Color::White();
 
