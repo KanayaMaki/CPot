d1f8a2e65cd50618863e9bb450e0b3e408c66d88
KanayaMaki <chlminami64@gmail.com>
2017-11-13 22:44:00 +0900

    Stringをマルチスレッド対応

--- Source/Atom/Standard/stringStandard.cpp
+++ Source/Atom/Standard/stringStandard.cpp
@@ -1,8 +1,6 @@
-//	ファイル名	:	Atom/Standard/string.cpp
-//	作者		:	齊藤芳紀
-//	作成日時	:	2017/11/09
-//	更新日時	:	2017/11/09
-//	内容		:	数字を文字列に変換する関数などの、std空間での実装
+//
+//	内容	：	Standardでの、文字列クラス
+//
 
 
 #include "./Atom/string.h"
@@ -14,60 +12,59 @@
 namespace cpot {
 
 
-//数字を文字列にしたときに、一時的に格納しておくバッファ
-static const s32 cToStringBufferSize = 100;
-static CHAR toStringBuffer[cToStringBufferSize];
-
-
-const CHAR* ToString(s32 aVal) {
+String<15> ToString(s32 aVal) {
+	CHAR toStringBuffer[16];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%d", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%d", aVal);
 	#endif
-	return toStringBuffer;
+	return String<15>(toStringBuffer);
 }
-const CHAR* ToString(u32 aVal) {
+String<15> ToString(u32 aVal) {
+	CHAR toStringBuffer[16];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%u", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%u", aVal);
 	#endif
-	return toStringBuffer;
+	return String<15>(toStringBuffer);
 }
-const CHAR* ToString(s64 aVal) {
+String<31> ToString(s64 aVal) {
+	CHAR toStringBuffer[32];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%lld", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%lld", aVal);
 	#endif
-	return toStringBuffer;
+	return String<31>(toStringBuffer);
 }
-const CHAR* ToString(u64 aVal) {
+String<31> ToString(u64 aVal) {
+	CHAR toStringBuffer[32];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%llu", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%llu", aVal);
 	#endif
-	return toStringBuffer;
+	return String<31>(toStringBuffer);
 }
-
-const CHAR* ToString(f32 aVal) {
+String<15> ToString(f32 aVal) {
+	CHAR toStringBuffer[16];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%f", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%f", aVal);
 	#endif
-	return toStringBuffer;
+	return String<15>(toStringBuffer);
 }
-
-const CHAR* ToString(f64 aVal) {
+String<31> ToString(f64 aVal) {
+	CHAR toStringBuffer[32];
 	#ifdef CPOT_ON_WINDOWS
 	sprintf_s(toStringBuffer, "%f", aVal);
 	#else
 	std::sprintf(toStringBuffer, "%f", aVal);
 	#endif
-	return toStringBuffer;
+	return String<31>(toStringBuffer);
 }
 
 }
--- Source/Atom/string.h
+++ Source/Atom/string.h
@@ -56,6 +56,10 @@ public:
 		return cSize;
 	}
 
+	operator const CHAR* () {
+		return mStr;
+	}
+
 	#pragma endregion
 
 
@@ -213,14 +217,12 @@ private:
 //数字を文字列にする
 #pragma region ToString
 
-//戻り値は一時バッファかもしれないので、呼び出し後すぐでないと中身の保証はされない
-
-const CHAR* ToString(s32 aVal);
-const CHAR* ToString(u32 aVal);
-const CHAR* ToString(s64 aVal);
-const CHAR* ToString(u64 aVal);
-const CHAR* ToString(f32 aVal);
-const CHAR* ToString(f64 aVal);
+String<15> ToString(s32 aVal);
+String<15> ToString(u32 aVal);
+String<31> ToString(s64 aVal);
+String<31> ToString(u64 aVal);
+String<15> ToString(f32 aVal);
+String<31> ToString(f64 aVal);
 
 #pragma endregion
 
